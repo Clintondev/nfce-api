33d41535ce304a5744700e32a168d3f4ce8c0306
Clinton Rodrigues <clintonrodrigues.dev@gmail.com>
2024-09-24 00:32:18 +0000

    edit params url

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const puppeteer = require('puppeteer');
 const app = express();
 const PORT = 3400;
 
-app.get('/nfce/:chaveAcesso', async (req, res) => {
+app.get('/nfce/:parametro', async (req, res) => {
   try {
-    const { chaveAcesso } = req.params;
+    const { parametro } = req.params;
 
-    const urlNfce = `http://nfe.sefaz.go.gov.br/nfeweb/sites/nfce/d/danfeNFCe?p=${chaveAcesso}%7C2%7C1%7C1%7Ce98ec3edfae819032a5b9514508e8c2383066480`;
+     // Codifica o parâmetro para garantir que os caracteres especiais sejam mantidos
+     const encodedParametro = encodeURIComponent(parametro);
+
+     // Use o parâmetro codificado diretamente na URL da NFC-e
+     const urlNfce = `http://nfe.sefaz.go.gov.br/nfeweb/sites/nfce/d/danfeNFCe?p=${encodedParametro}`;
+ 
+     // Log da URL para verificar se está correta
+     console.log(urlNfce);
+ 
 
     const browser = await puppeteer.launch({
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
@@ -34,83 +42,80 @@ app.get('/nfce/:chaveAcesso', async (req, res) => {
     await page.goto(iframeUrl, { waitUntil: 'networkidle2' });
 
     const nfceData = await page.evaluate(() => {
-        const cleanText = (text) => {
-          return text ? text.replace(/\n/g, ' ').replace(/\s+/g, ' ').trim() : null;
-        };
-      
-        const vendorName = cleanText(document.querySelector('#u20')?.textContent);
-        const vendorCNPJ = cleanText(document.querySelector('.text')?.textContent.replace('CNPJ:', '').trim());
-        const vendorAddress = cleanText(document.querySelectorAll('.text')[1]?.textContent);
-      
-        const items = [];
-        const itemRows = document.querySelectorAll('#tabResult tr');
-        itemRows.forEach(row => {
-          const itemName = cleanText(row.querySelector('.txtTit')?.textContent);
-          const itemCode = cleanText(row.querySelector('.RCod')?.textContent.replace('(Código:', '').replace(')', ''));
-          const quantity = cleanText(row.querySelector('.Rqtd')?.textContent.replace('Qtde.:', ''));
-          const unit = cleanText(row.querySelector('.RUN')?.textContent.replace('UN:', ''));
-          const unitPrice = cleanText(row.querySelector('.RvlUnit')?.textContent.replace('Vl. Unit.:', ''));
-          const totalPrice = cleanText(row.querySelector('.valor')?.textContent);
-      
-          if (itemName && quantity && unitPrice && totalPrice) {
-            items.push({
-              name: itemName,
-              code: itemCode,
-              quantity: quantity,
-              unit: unit,
-              unitPrice: unitPrice,
-              totalPrice: totalPrice
-            });
-          }
-        });
-      
-        const totalItems = cleanText(document.querySelector('#totalNota .totalNumb')?.textContent);
-        const totalAmount = cleanText(document.querySelector('#totalNota .txtMax')?.textContent);
-        const paymentMethod = cleanText(document.querySelector('#linhaTotal .tx')?.textContent) || 'N/A';
-        const totalPaid = cleanText(document.querySelectorAll('#linhaTotal .totalNumb')[1]?.textContent);
-        const change = cleanText(document.querySelectorAll('#linhaTotal .totalNumb')[2]?.textContent) || 'N/A';
-      
-        const invoiceInfoText = document.querySelector('#infos ul li')?.textContent || '';
-        const invoiceNumber = invoiceInfoText.match(/Número:\s*(\d+)/)?.[1] || null;
-        const invoiceSeries = invoiceInfoText.match(/Série:\s*(\d+)/)?.[1] || null;
-        const invoiceEmission = invoiceInfoText
-        .match(/Emissão:\s*([\d\/\s\:\-]+)/)?.[1]
-        ?.replace(/[\n\t]+/g, ' ')  
-        .trim();
-              const authorizationProtocol = invoiceInfoText.match(/Protocolo de Autorização:\s*(\d+)/)?.[1] || null;
-        const xmlVersion = invoiceInfoText.match(/Versão XML:\s*(\d+\.\d+)/)?.[1] || null;
-        const xsltVersion = invoiceInfoText.match(/Versão XSLT:\s*(\d+\.\d+)/)?.[1] || null;
-      
-        const accessKey = cleanText(document.querySelector('.chave')?.textContent);
-        const consumerInfo = cleanText(document.querySelector('#infos ul li')?.textContent) || 'Consumidor não identificado';
-      
-        return {
-          vendor: {
-            name: vendorName,
-            CNPJ: vendorCNPJ,
-            address: vendorAddress,
-          },
-          items,
-          totalInfo: {
-            totalItems,
-            totalAmount,
-            paymentMethod,
-            totalPaid,
-            change,
-          },
-          generalInfo: {
-            number: invoiceNumber,
-            series: invoiceSeries,
-            emission: invoiceEmission,
-            authorizationProtocol,
-            xmlVersion,
-            xsltVersion,
-          },
-          accessKey,
-          consumerInfo,
-        };
+      const cleanText = (text) => text ? text.replace(/\n/g, ' ').replace(/\s+/g, ' ').trim() : null;
+
+      const vendorName = cleanText(document.querySelector('#u20')?.textContent);
+      const vendorCNPJ = cleanText(document.querySelector('.text')?.textContent.replace('CNPJ:', '').trim());
+      const vendorAddress = cleanText(document.querySelectorAll('.text')[1]?.textContent);
+
+      const items = [];
+      const itemRows = document.querySelectorAll('#tabResult tr');
+      itemRows.forEach(row => {
+        const itemName = cleanText(row.querySelector('.txtTit')?.textContent);
+        const itemCode = cleanText(row.querySelector('.RCod')?.textContent.replace('(Código:', '').replace(')', ''));
+        const quantity = cleanText(row.querySelector('.Rqtd')?.textContent.replace('Qtde.:', ''));
+        const unit = cleanText(row.querySelector('.RUN')?.textContent.replace('UN:', ''));
+        const unitPrice = cleanText(row.querySelector('.RvlUnit')?.textContent.replace('Vl. Unit.:', ''));
+        const totalPrice = cleanText(row.querySelector('.valor')?.textContent);
+
+        if (itemName && quantity && unitPrice && totalPrice) {
+          items.push({
+            name: itemName,
+            code: itemCode,
+            quantity: quantity,
+            unit: unit,
+            unitPrice: unitPrice,
+            totalPrice: totalPrice
+          });
+        }
       });
-      
+
+      const totalItems = cleanText(document.querySelector('#totalNota .totalNumb')?.textContent);
+      const totalAmount = cleanText(document.querySelector('#totalNota .txtMax')?.textContent);
+      const paymentMethod = cleanText(document.querySelector('#linhaTotal .tx')?.textContent) || 'N/A';
+      const totalPaid = cleanText(document.querySelectorAll('#linhaTotal .totalNumb')[1]?.textContent);
+      const change = cleanText(document.querySelectorAll('#linhaTotal .totalNumb')[2]?.textContent) || 'N/A';
+
+      const invoiceInfoText = document.querySelector('#infos ul li')?.textContent || '';
+      const invoiceNumber = invoiceInfoText.match(/Número:\s*(\d+)/)?.[1] || null;
+      const invoiceSeries = invoiceInfoText.match(/Série:\s*(\d+)/)?.[1] || null;
+      const invoiceEmission = invoiceInfoText
+        .match(/Emissão:\s*([\d\/\s\:\-]+)/)?.[1]
+        ?.replace(/[\n\t]+/g, ' ')
+        .trim();
+      const authorizationProtocol = invoiceInfoText.match(/Protocolo de Autorização:\s*(\d+)/)?.[1] || null;
+      const xmlVersion = invoiceInfoText.match(/Versão XML:\s*(\d+\.\d+)/)?.[1] || null;
+      const xsltVersion = invoiceInfoText.match(/Versão XSLT:\s*(\d+\.\d+)/)?.[1] || null;
+
+      const accessKey = cleanText(document.querySelector('.chave')?.textContent);
+      const consumerInfo = cleanText(document.querySelector('#infos ul li')?.textContent) || 'Consumidor não identificado';
+
+      return {
+        vendor: {
+          name: vendorName,
+          CNPJ: vendorCNPJ,
+          address: vendorAddress,
+        },
+        items,
+        totalInfo: {
+          totalItems,
+          totalAmount,
+          paymentMethod,
+          totalPaid,
+          change,
+        },
+        generalInfo: {
+          number: invoiceNumber,
+          series: invoiceSeries,
+          emission: invoiceEmission,
+          authorizationProtocol,
+          xmlVersion,
+          xsltVersion,
+        },
+        accessKey,
+        consumerInfo,
+      };
+    });
 
     await browser.close();
 
